a781f5e23a360604210fc7d49a07cf78f8714788
lostal <alvarolostal04@gmail.com>
2025-12-23 20:18:25 +0100

    maskable icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,8 @@ export default defineConfig({
         name: 'Álvaro Lostal',
         short_name: 'Lostal',
         description: 'Portafolio profesional de Álvaro Lostal',
-        theme_color: '#000000',
-        background_color: '#000000',
+        theme_color: '#fafaf9',
+        background_color: '#fafaf9',
         display: 'standalone',
         icons: [
           {
@@ -33,6 +33,12 @@ export default defineConfig({
             src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+          },
+          {
+            src: '/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
